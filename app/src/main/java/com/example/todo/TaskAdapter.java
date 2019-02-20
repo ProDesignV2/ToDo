14de8278976226d0d6786eeb5c0588dff5f0abc2
ProDesignV2 <eoinpgogarty@gmail.com>
2019-02-20 11:07:42 +0000

    Added checkbox

--- app/src/main/java/com/example/todo/TaskAdapter.java
+++ app/src/main/java/com/example/todo/TaskAdapter.java
@@ -5,6 +5,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.TextView;
 
 import com.example.todo.models.Tasks;
@@ -16,17 +18,29 @@ class TaskAdapter extends ArrayAdapter<String> {
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
+    public View getView(final int position, View convertView, ViewGroup parent) {
         LayoutInflater taskInflater = LayoutInflater.from(getContext());
         View taskView = taskInflater.inflate(R.layout.task_row, parent, false);
 
-        TextView nameText = taskView.findViewById(R.id.nameView);
+        final TextView nameText = taskView.findViewById(R.id.nameView);
         TextView dateText = taskView.findViewById(R.id.dateView);
+        CheckBox checked = taskView.findViewById(R.id.taskChecked);
 
         // Set TextViews from database
-        long row_id = position + 1;
+        final long row_id = position + 1;
         nameText.setText(Tasks.findById(Tasks.class,row_id).getName());
         dateText.setText(String.valueOf(Tasks.findById(Tasks.class,row_id).dateUntil()));
+
+        checked.setOnCheckedChangeListener(
+                new CompoundButton.OnCheckedChangeListener() {
+                    @Override
+                    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                        Tasks.findById(Tasks.class,row_id).setChecked(isChecked);
+                        // Change this to grey out
+                        nameText.setAllCaps(isChecked);
+                    }
+                }
+        );
 
         return taskView;
     }
